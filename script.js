858067e2d55932df89bc5f2f03765859734ec331
raihanhn <raihanhn20@gmail.com>
2023-02-02 14:33:10 +0600

    6th commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+     cartItem.classList.remove('active');
 }
 
 
@@ -9,6 +11,8 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 
@@ -16,4 +20,24 @@ let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
+
+
+window.onscroll = () =>{
+     navbar.classList.remove('active');
+     searchForm.classList.remove('active');
+     cartItem.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
